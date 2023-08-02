ca6ac23e13e0886fe7d85ddfe174dccaa7e2fa1c
Talha Ghauri <talhaghauridev@gmail.com>
2023-08-03 00:34:50 +0500

    add

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+import Banner from "../../components/Banner";
+import Navigation from "../../components/Navigation";
+
+const Home = () => {
+  return (
+    <>
+      <main id="home" className="relative overflow-hidden backgro"  >
+        <Navigation />
+        <Banner />
+      </main>
+    </>
+  );
+};
+
+export default Home;
